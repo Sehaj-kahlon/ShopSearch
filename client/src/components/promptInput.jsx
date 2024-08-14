276132fc8fbcadf8b6e9fa6557ca75f372b2b73c
Sehaj-kahlon <sehajkahlon437@gmail.com>
2024-08-15 03:39:01 +0530

    home page designed

--- client/src/components/promptInput.jsx
+++ client/src/components/promptInput.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "./components.css";
+import { Input, Button, Form } from "antd";
 function PromptInput() {
-  return <div>prompt input</div>;
+  const { TextArea } = Input;
+  return (
+    <div>
+      <Form>
+        <h5>Describe the item you're searching for...</h5>
+        <div className="textarea-container">
+          <TextArea
+            placeholder="Enter text here"
+            autoSize={{ minRows: 6, maxRows: 10 }}
+            className="inputBox"
+            required
+          ></TextArea>
+          <small className="help-text">
+            Provide details like color, style, and features to get better search
+            results.
+          </small>
+        </div>
+        <button type="submit" className="btn btn-primary find-button">
+          Find Items
+        </button>
+      </Form>
+    </div>
+  );
 }
 
 export default PromptInput;
