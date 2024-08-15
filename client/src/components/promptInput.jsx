c3b8ccaa6c5f409aede3748fee76fd94289c1832
Sehaj-kahlon <sehajkahlon437@gmail.com>
2024-08-15 18:37:12 +0530

    working on backend

--- client/src/components/promptInput.jsx
+++ client/src/components/promptInput.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./components.css";
-import { Input, Button, Form } from "antd";
+import { Input, Form } from "antd";
 function PromptInput() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
   const { TextArea } = Input;
+  // const BASE_URL = "http://localhost:5000";
+  const getResult = async () => {
+    console.log(prompt);
+    try {
+      const response = await fetch("http://localhost:5000/getresult", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ prompt }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setResult(data.result);
+      } else {
+        setError("Network call Failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError(error.message);
+    }
+  };
+  const handleInputChange = (e) => {
+    setPrompt(e.target.value);
+  };
   return (
     <div>
       <Form>
-        <h5>Describe the item you're searching for...</h5>
+        <h5>Describe the item you're searching for</h5>
         <div className="textarea-container">
           <TextArea
             placeholder="Enter text here"
             autoSize={{ minRows: 6, maxRows: 10 }}
             className="inputBox"
+            onChange={handleInputChange}
             required
           ></TextArea>
           <small className="help-text">
@@ -19,10 +46,16 @@ function PromptInput() {
             results.
           </small>
         </div>
-        <button type="submit" className="btn btn-primary find-button">
+        <button
+          type="submit"
+          className="btn btn-primary find-button"
+          onClick={getResult}
+        >
           Find Items
         </button>
       </Form>
+      {result}
+      {error}
     </div>
   );
 }
--- flask-server/localhost.py
+++ flask-server/localhost.py
@@ -3,25 +3,12 @@ from flask_cors import CORS
 
 app  = Flask(__name__)
 CORS(app)
-@app.route("/useEffectCall", methods = ['GET'])
-def function():
-    data = "Hello World"
-    print(data)
-    return jsonify({'data': data})
-@app.route("/buttonCall", methods = ['GET'])
-def members():
-    data = "1000"
-    print(data)
-    return jsonify({'data': data})
-@app.route("/postCall", methods = ['POST'])
-def SUM():
-    
+@app.route("/getresult", methods = ['POST', 'OPTIONS'])
+def SUM(): 
     try:
         data = request.get_json()
-        numberA = data.get('numberA')
-        numberB = data.get('numberB')
-        result  =numberA + numberB
-        return jsonify({'result': result}), 200
+        print(data)
+        return jsonify({'result': "hello"}), 200
     except Exception as e:  
         return jsonify({'error': str(e)}), 400
     
